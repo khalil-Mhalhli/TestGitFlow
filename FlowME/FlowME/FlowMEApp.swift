6fd7b625514e3cdbe5b181b5f4fedd90ea066d16
Kahlil <khalil.mhalhli@apimo.com>
2024-08-01 15:58:26 +0200

    first commit

---
+++ FlowME/FlowME/FlowMEApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlowMEApp.swift
+//  FlowME
+//
+//  Created by Khalil Mhalhli on 01/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct FlowMEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
